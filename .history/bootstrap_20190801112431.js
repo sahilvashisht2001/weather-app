07dd752966989a120e41a6c1871ba04a781bd8a6
Sahil Vashisht <755857@student.tor.loc>
2019-08-01 11:26:58 -0400

    first commit

---
+++ .history/bootstrap_20190801112431.js
@@ -0,0 +1,54 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+input {font-weight:bold;}
+input[type="text"], textarea {
+ 
+background-color : #d1d1d1; 
+ 
+}
+ 
+input, select, textarea{
+ color: #000080;
+}
+ 
+textarea:focus, input:focus {
+ color: #000080;
+}
+ 
+.buttonCss {
+ background-color: #008080;
+ border: none;
+ color: white;
+ padding: 15px 32px;
+ text-align: center;
+ text-decoration: none;
+ display: inline-block;
+ font-size: 16px;
+ margin: 4px 2px;
+ cursor: pointer;
+}
